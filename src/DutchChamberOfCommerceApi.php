423ecb81f7730bae0337dc22ac561afaadcc7715
Meta Bergman <m.bergman@daadkracht-marketing.nl>
2024-02-15 10:51:19 +0100

    feat: Add searchRequest, baseProfileRequest, baseProfileBranchesRequest, and branchProfileRequest methods

--- src/DutchChamberOfCommerceApi.php
+++ src/DutchChamberOfCommerceApi.php
@@ -4,4 +4,23 @@ namespace DaadkrachtMarketing\DutchChamberOfCommerceApi;
 
 class DutchChamberOfCommerceApi
 {
+    public function searchRequest($testMode = false): Requests\Search\SearchRequest
+    {
+        return new Requests\Search\SearchRequest($testMode);
+    }
+
+    public function baseProfileRequest($testMode = false): Requests\BaseProfile\BaseProfileRequest
+    {
+        return new Requests\BaseProfile\BaseProfileRequest($testMode);
+    }
+
+    public function baseProfileBranchesRequest($testMode = false): Requests\BaseProfileBranches\BaseProfileBranchesRequest
+    {
+        return new Requests\BaseProfileBranches\BaseProfileBranchesRequest($testMode);
+    }
+
+    public function branchProfileRequest($testMode = false): Requests\BranchProfile\BranchProfileRequest
+    {
+        return new Requests\BranchProfile\BranchProfileRequest($testMode);
+    }
 }
